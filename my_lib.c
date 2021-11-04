8c9ceb57483d9e3195b958b590e2cecf604ebfe4
GregoriSerra <93575453+GregoriSerra@users.noreply.github.com>
2021-11-04 11:26:05 +0100

    Add files via upload

--- my_lib.c
+++ my_lib.c
@@ -2,37 +2,45 @@
 
 size_t my_strlen(const char *str) {
   size_t len = 0;
-  while (*str) { // Check if the content is 0
-      len++;
-      str++; // Increment the pointer to the next char
+  int i = 0;
+  while (str[i]) { //Mentre no final, itinera
+          i++;
+          len++;
   }
   return len;
 }
 
 int my_strcmp(const char *str1, const char *str2) {
     int i;
-
-    for (i = 0; str1[i] == str2[i]; i++)
-        if (str1[i] == '\0')
+    i = 0;
+    while (str1[i] == str2[i]){ //Mentre siguin iguals, itineram
+        if (str1[i] == '\0'){ //Retorna 0 si dos strings iguals
             return 0;
-    return str1[i] - str2[i];
+        }
+        i++;
+    }
+    return str1[i] - str2[i]; //Quan trobi una diferència, retornarà la diferència
 }
 
 char *my_strcpy(char *dest, const char *src){ 
     int i;
-    for(i=0; src[i] != '\0'; i++){
+    i=0;
+    while (src[i] != '\0'){ //Mentre no s'arribi al final, itinera i copia char a char
         dest[i] = src [i];
+        i++;
     }
-    dest[i] = '\0';
+    dest[i] = '\0'; //Tancam el string destí
     return dest;
 }
 
 char *my_strncpy(char *dest, const char *src, size_t n){
     size_t i;
-    for( i = 0; i < n && src[i] != '\0'; i++){
-        dest[i] = src[i];
+    i=0;
+    while (i < n && src[i] != '\0') { //Itineram fins final de src si n>i, o fins quantitat desitjada de caràcters a copiar
+        dest[i] = src[i]; //Copiam char a char en dest
+        i++;
     }
-    while (i < n) { 
+    while (i < n) { //Si length de src < n, omplim amb 0's
         dest[i] = '\0';
     }
     return dest;
@@ -40,19 +48,27 @@ char *my_strncpy(char *dest, const char *src, size_t n){
 
 char *my_strcat(char *dest, const char *src){
     int x, y;
-    for (x=0; dest[x] != '\0'; x++);
-    for (y=0; src[y] != '\0'; y++)
+    x=0;
+    while(dest[x] != '\0'){ //Trobam length de dest
+        x++;
+    }
+    y=0;
+    while(src[y] != '\0'){ //Amb y, itineram fins a arribar al final de src. A la vegada, al final del string copiam char a char el string src.
         dest[x+y] = src[y];
-    dest[x+y] = '\0';
+        y++;
+    }
+    dest[x+y] = '\0'; //Tancam el string dest
     return dest;
 }
 
 char *my_strchr(const char *s, int c){
-int i = 0;
-    while (s[i] != c && s[i] != '\0')
-        i++;
-    if (s[i] == c)
+int i;
+i=0;
+while(s[i] != c && s[i] != '\0'){ //Mentre char no trobat i no final itinera
+    i++;
+}
+if (s[i] == c) //Si char trobat, retorna el valor de la posició s[i]
         return &s[i];
     else
-        return NULL;
+        return NULL; //Si no trobat, retorna NULL
 }
